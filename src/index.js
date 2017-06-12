a62b7e1a843ff76f4117d1a439061837c9ddfc2a
Kishlin <legrand.pierrelouis@gmail.com>
2017-06-13 00:34:14 +0200

    Components and Props.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatName(user) {
-	return user.firstName + ' ' + user.lastName;
-}
-
-const user = {
-	firstName: 'Pierre-Louis',
-	lastName: 'Legrand'
-};
-
-function tick() {
-	const element = (
-		<div>
-			<h1>Hello, {formatName(user)}!</h1>
-			<h2>It is {new Date().toLocaleTimeString()}.</h2>
+function Comment(props) {
+	return (
+		<div className="Comment">
+			<div className="UserInfor">
+				<img className="Avatar"
+					src={props.author.avatarUrl}
+					alt={props.author.name}
+				/>
+				<div className="UserInfo-name">
+					{props.author.name}
+				</div>
+			</div>
+			<div className="Comment-text">
+				{props.text}
+			</div>
+			<div className="Comment-date">
+				{formatDate(props.date)}
+			</div>
 		</div>
-		
-	);
-
-	ReactDOM.render(
-	  element,
-	  document.getElementById('root')
 	);
 }
 
-setInterval(tick, 1000);
+function Welcome(props) {
+	return <h1>Hello, {props.name}!</h1>;
+}
+
+function App() {
+	return (
+		<div>
+			<Welcome name="Sarah" />
+			<Welcome name="Laura" />
+			<Welcome name="Rebecca" />
+		</div>
+	);
+}
+
+const element = <Welcome name="Sarah" />;
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
