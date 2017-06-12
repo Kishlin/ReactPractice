562c1a296bb2882eed4677471c2d9a4c98a778b4
Kishlin <legrand.pierrelouis@gmail.com>
2017-06-13 00:21:21 +0200

    Ticking clock.

--- src/index.js
+++ src/index.js
@@ -10,13 +10,19 @@ const user = {
 	lastName: 'Legrand'
 };
 
-const element = (
-	<h1>
-		Hello, {formatName(user)}!
-	</h1>
-);
+function tick() {
+	const element = (
+		<div>
+			<h1>Hello, {formatName(user)}!</h1>
+			<h2>It is {new Date().toLocaleTimeString()}.</h2>
+		</div>
+		
+	);
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+	ReactDOM.render(
+	  element,
+	  document.getElementById('root')
+	);
+}
+
+setInterval(tick, 1000);
